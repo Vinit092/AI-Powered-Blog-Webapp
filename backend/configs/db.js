4fe9509913a9f9ce446409c567643645abc138b1
vinit092 <vinitsuva97@gmail.com>
2025-07-12 14:44:19 +0530

    updated error

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
     const connectDB = async ()=>{
     try {
+        if (mongoose.connections[0].readyState) return;
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
